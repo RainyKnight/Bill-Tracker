2f0c2caaf50c87d98ac400b36be53dd6870ab139
RainyKnight <stoltmanlucas@gmail.com>
2020-07-01 01:40:51 -0700

    Initial check-in of module Bill-Tracker

---
+++ Bill-Tracker/Program.cs
@@ -0,0 +1,32 @@
+﻿// Bill Tracking Application
+// Created by Ahmed Khan and Lucas Stoltman
+// 2020
+using System;
+
+namespace Bill_Tracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+
+
+    class Bill
+    {
+
+
+        
+    }
+
+    class User
+    {
+
+
+
+    }
+}

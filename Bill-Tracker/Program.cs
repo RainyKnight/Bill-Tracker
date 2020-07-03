168cbc36439fb24662ba2147dafdfe80b123671a
RainyKnight <stoltmanlucas@gmail.com>
2020-07-02 19:20:30 -0700

    Removed conflicting classes from program.cs

--- Bill-Tracker/Program.cs
+++ Bill-Tracker/Program.cs
@@ -14,37 +14,4 @@ namespace Bill_Tracker
 
         }
     }
-
-    class Bill
-    {
-  
-        private string name;
-        private double monthlyCost;
-        private double annualCost;
-
-        // days in a billing cycle
-        private int period;
-
-        // days left until billed
-        private int daysLeft;
-
-        // TODO constructor
-
-        // TODO get/set functions
-
-    }
-
-    class User
-    {
-        private string name;
-        private string password;
-
-        // array of bill objects to store
-        // TODO will probably change to a linkedlist for scalability
-        private Bill[] Bill;
-
-        // TODO constructor
-
-        // TODO get/set functions
-    }
 }

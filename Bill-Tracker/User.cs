3464540690b773df4c5f24ff898b26b7b7f45720
Ahmed Khan <51451785+ahmedisok@users.noreply.github.com>
2020-07-02 17:49:26 -0700

    Add files via upload

---
+++ Bill-Tracker/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Bill_Tracker
+{
+    public class User
+    {
+        private string name;
+        private string password;
+        private int numberofBills;
+        private Bill[] Bill;
+
+        public User(string name, string password, int numberofBills)
+        {
+            this.numberofBills = numberofBills;
+            this.name = name;
+            this.password = password;
+        }
+
+        public void addBill(int numberofBills)
+        { 
+            Bill[] temp = new Bill[numberofBills + 1];
+            temp = Bill;
+        }
+
+
+
+        // TODO get/set functions
+    }
+}
